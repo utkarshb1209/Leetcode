7126e3ea29c1aeca72cba07ab8ea78f3c2901cb1
Utkarsh Bajpai <91046232+utkarshb1209@users.noreply.github.com>
2024-03-09 11:22:57 +0530

    Time: 47 ms (74.95%), Space: 49 MB (5.14%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        var s="Hello World";
+        return s;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
