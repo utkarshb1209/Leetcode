e62127a31d2ca3b3c3d12cc4a8653d7ccdadee38
Utkarsh Bajpai <91046232+utkarshb1209@users.noreply.github.com>
2024-05-08 13:14:06 +0530

    Time: 5 ms (24.72%), Space: 11.7 MB (79.85%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,6 +14,6 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q) return 1;
         if(!p || !q || p->val!=q->val) return 0;
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        return isSameTree(p->left,q->left) && isSameTree(p->right, q->right);
     }
 };
